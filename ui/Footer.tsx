1f518e41b8c7eb78cd92b8aa5bb600aac30fe443
Tagir Almasov <tccup1@gmail.com>
2021-11-07 20:51:21 +0000

    add contacts

--- ui/Footer.tsx
+++ ui/Footer.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import MailIcon from '@mui/icons-material/MailOutline';
 import Image from 'next/image'
 import Link from './Link'
-import { Box, Container, Divider, Grid } from '@mui/material'
+import { Box, Container, Divider, Grid, List, ListItem } from '@mui/material'
 
 
 interface Props {
@@ -14,7 +16,17 @@ export const Footer = (props: Props) => {
       <Divider sx={{ mt: 4 }} />
       <Container maxWidth='md' sx={{ my: 2 }}>
         <Box display='flex' justifyContent='space-between' alignItems='center'>
-          <Link href='/about'>О сайте</Link>
+          <List>
+            <ListItem>
+              <Link href='/about'>О сайте</Link>
+            </ListItem>
+            <ListItem>
+
+              <Link href='mailto:bk-korona-rate@tagir-a.com'><MailIcon /></Link>
+              <Box sx={{ ml: 1 }} />
+              <Link rel="noreferrer" href='https://instagram.com/netyaneangel'><InstagramIcon /></Link>
+            </ListItem>
+          </List>
           <Image src="/made-in-russia-sign-ru.png" alt='Знак "Сделано в России"' width={233} height={70} />
         </Box>
       </Container>
